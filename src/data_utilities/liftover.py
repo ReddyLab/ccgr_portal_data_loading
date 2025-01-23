7e6266b69c5a93fec8a48aed4aa46da5a9bab919
Thomas Cowart <thomas.cowart@duke.edu>
2025-01-23 10:35:02 -0500

    Handle gene edge cases
    There are a few  liftover edge cases we need to handle for hg19->hg38:
    1. Sometimes the hg19 gene doesn't exist at all in hg38
    2. Sometimes the hg19 gene got renamed
    3. Sometimes the gene's ensembl id changed(!)
    
    These changes do some refactoring as well.

--- src/data_utilities/liftover.py
+++ src/data_utilities/liftover.py
@@ -3,6 +3,9 @@
 import csv
 import argparse
 
+from collections import defaultdict
+from importlib.resources import files
+
 from pyliftre import Mapper
 
 
@@ -17,37 +20,61 @@ def merge_mapping(mapping):
     return new_mapping
 
 
-class Experiment:
-    def __init__(self, experiment_file, output, unmapped_file, mapper):
-        self.experiment_file = csv.DictReader(experiment_file, delimiter="\t")
+class Liftover:
+    fieldnames = ()
+
+    def __init__(self, liftover_file, output, unmapped_file, summary_file, mapper, **kwargs):
+        self.liftover_file = csv.DictReader(liftover_file, delimiter="\t")
         self.writer = csv.DictWriter(
             output,
-            fieldnames=(
-                "chrom",
-                "start",
-                "end",
-                "strand",
-                "parent_chrom",
-                "parent_start",
-                "parent_end",
-                "parent_strand",
-                "facets",
-                "misc",
-            ),
+            fieldnames=self.fieldnames,
             delimiter="\t",
         )
         self.writer.writeheader()
         self.unmapped_file = unmapped_file
+        self.summary_file = summary_file
         self.mapper = mapper
+        self.mapped_count = 0
+        self.unmapped_count = 0
+        self.unmapped_genes = None
 
-    def _handle_unmapped(self, line):
-        if self.unmapped_file:
-            self.unmapped_file.write(line)
-        else:
-            print(line)
+    def _handle_unmapped(self, line=None):
+        self.unmapped_count += 1
+        if line is not None:
+            if self.unmapped_file:
+                self.unmapped_file.write(line)
+            else:
+                print(line)
+
+    def _write_mapped(self, data):
+        self.writer.writerow(data)
+        self.mapped_count += 1
+
+    def write_summary(self):
+        if self.summary_file:
+            summary = f"Mapped Coordinates: {self.mapped_count}\nUnmapped Coordinates: {self.unmapped_count}\n"
+            if self.unmapped_genes is not None:
+                summary += f"Unmapped Genes: {self.unmapped_genes}\n"
+
+            self.summary_file.write(summary)
+
+
+class Experiment(Liftover):
+    fieldnames = (
+        "chrom",
+        "start",
+        "end",
+        "strand",
+        "parent_chrom",
+        "parent_start",
+        "parent_end",
+        "parent_strand",
+        "facets",
+        "misc",
+    )
 
     def liftover(self):
-        for row in self.experiment_file:
+        for row in self.liftover_file:
             strand = row["strand"]
             if strand == ".":
                 strand = "+"
@@ -135,40 +162,68 @@ class Experiment:
                 no_parent = True
 
             if "chrom" in new_row and (no_parent or "parent_chrom" in new_row):
-                self.writer.writerow(new_row)
+                self._write_mapped(new_row)
+
+        self.write_summary()
 
 
-class Analysis:
-    def __init__(self, analysis_file, output, unmapped_file, mapper):
-        self.analysis_file = csv.DictReader(analysis_file, delimiter="\t")
-        self.writer = csv.DictWriter(
-            output,
-            fieldnames=(
-                "chrom",
-                "start",
-                "end",
-                "strand",
-                "gene_name",
-                "gene_ensembl_id",
-                "raw_p_val",
-                "adj_p_val",
-                "effect_size",
-                "facets",
-            ),
-            delimiter="\t",
-        )
-        self.writer.writeheader()
-        self.unmapped_file = unmapped_file
-        self.mapper = mapper
+class Analysis(Liftover):
+    fieldnames = (
+        "chrom",
+        "start",
+        "end",
+        "strand",
+        "gene_name",
+        "gene_ensembl_id",
+        "raw_p_val",
+        "adj_p_val",
+        "effect_size",
+        "facets",
+    )
 
-    def _handle_unmapped(self, line):
-        if self.unmapped_file:
-            self.unmapped_file.write(line)
-        else:
-            print(line)
+    def __init__(self, liftover_file, output, unmapped_file, summary_file, mapper, **kwargs):
+        super().__init__(liftover_file, output, unmapped_file, summary_file, mapper, **kwargs)
+        self.hg38_genes = defaultdict(set)
+        self.hg19_genes = {}
+        for gene in kwargs["genes"].readlines():
+            hg38_gene, ensembl, hg19_gene = gene.strip().split("\t")
+            self.hg38_genes[hg38_gene].add(ensembl)
+            if hg19_gene != "NULL":
+                self.hg19_genes[(hg19_gene, ensembl)] = hg38_gene
+        self.unmapped_genes = 0
 
     def liftover(self):
-        for row in self.analysis_file:
+        for row in self.liftover_file:
+            ensembl_id = row["gene_ensembl_id"]
+            gene_name = row["gene_name"]
+            if gene_name != "":
+                if gene_name not in self.hg38_genes:
+                    # Check if the hg19 gene name/ensembl id pair match an hg38 gene name.
+                    # If so, use that hg38 gene name.
+                    if (gene_name, ensembl_id) in self.hg19_genes:
+                        gene_name = self.hg19_genes[(gene_name, ensembl_id)]
+                    else:
+                        self.unmapped_genes += 1
+                        self._handle_unmapped(
+                            f"{row['chrom']}\t{row['start']}\t{row['end']}\t{row['strand']}\t{gene_name}\t{row['gene_ensembl_id']}\n"
+                        )
+                        continue
+                elif gene_name in self.hg38_genes:
+                    ensembl_ids = self.hg38_genes[gene_name]
+                    if ensembl_id not in ensembl_ids and len(ensembl_ids) == 1:
+                        # In this case the gene has a new ensembl id
+                        ensembl_id = list(ensembl_ids)[0]
+                    elif len(ensembl_ids) > 1:
+                        # In this case the gene has a new ensembl id, but in hg38 there
+                        # are multiple ensembl ids to choose from, so we don't know which
+                        # one is right and just don't map it
+
+                        self.unmapped_genes += 1
+                        self._handle_unmapped(
+                            f"{row['chrom']}\t{row['start']}\t{row['end']}\t{row['strand']}\t{gene_name}\t{row['gene_ensembl_id']}\n"
+                        )
+                        continue
+
             strand = row["strand"]
             if strand == ".":
                 strand = "+"
@@ -197,14 +252,14 @@ class Analysis:
                         self._handle_unmapped(f"{row['chrom']}\t{row['start']}\t{row['end']}\t{strand}\n")
                         continue
 
-                    self.writer.writerow(
+                    self._write_mapped(
                         {
                             "chrom": new_coords[0],
                             "start": new_coords[1],
                             "end": new_coords[2],
                             "strand": new_coords[3] if row["strand"] != "." else ".",
-                            "gene_name": row["gene_name"],
-                            "gene_ensembl_id": row["gene_ensembl_id"],
+                            "gene_name": gene_name,
+                            "gene_ensembl_id": ensembl_id,
                             "raw_p_val": row["raw_p_val"],
                             "adj_p_val": row["adj_p_val"],
                             "effect_size": row["effect_size"],
@@ -212,11 +267,14 @@ class Analysis:
                         }
                     )
 
+        self.write_summary()
+
 
 def get_args():
     parser = argparse.ArgumentParser("A tool for lifting over experiment and analysis data")
     parser.add_argument("-c", "--chain", required=True, help="The chain file containing the liftover data")
     parser.add_argument("-u", "--unmapped", type=argparse.FileType("w"), help="The file for logging unmapped regions")
+    parser.add_argument("-s", "--summary", type=argparse.FileType("w"), help="A summary of the mapping results")
     parser.add_argument(
         "-o", "--output", type=argparse.FileType("w"), help="The output file with the lifted over regions"
     )
@@ -231,9 +289,11 @@ def get_args():
 def run(args):
     mapper = Mapper(args.chain)
     if args.experiment:
-        regions = Experiment(args.experiment, args.output, args.unmapped, mapper)
+        regions = Experiment(args.experiment, args.output, args.unmapped, args.summary, mapper)
     elif args.analysis:
-        regions = Analysis(args.analysis, args.output, args.unmapped, mapper)
+        genes_file = files("data_utilities").joinpath("hg38_genes.tsv")
+        with open(genes_file, encoding="utf-8") as genes:
+            regions = Analysis(args.analysis, args.output, args.unmapped, args.summary, mapper, genes=genes)
 
     regions.liftover()
 
